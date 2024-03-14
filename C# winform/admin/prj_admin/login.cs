bc80536fceb30e621bb9d549c0a8b423d8dba08b
jykimqwer <qktpdqktpd@daum.net>
2024-03-14 16:30:06 +0900

    ParkingManagementSystem

---
+++ C# winform/admin/prj_admin/login.cs
@@ -0,0 +1,113 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prj_admin
+{
+    public partial class login : Form
+    {
+        string connectionString = "Server=127.0.0.1;Database=plate;Uid=root;Pwd=1234;";
+        MySqlConnection connection;
+        public login()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection(connectionString); 
+            loginPW.PasswordChar = '*';
+            this.KeyPreview = true; // KeyPreview 속성을 true로 설정
+            this.KeyDown += new KeyEventHandler(Main_KeyDown); //
+        }
+        private bool CheckID_PW(MySqlConnection connection, string id, string pw)
+        {
+            string query = "SELECT COUNT(*) FROM admin_info WHERE ID = @id AND PW = @pw";
+            if(connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            using (MySqlCommand cmd = new MySqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@pw", pw);
+                try
+                {
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        private void loginDB(object sender, EventArgs e)
+        {
+            string id = loginID.Text;
+            string pw = loginPW.Text;
+            try
+            {
+                connection.Open();
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    if (CheckID_PW(connection, id, pw))
+                    {
+                        MessageBox.Show("MySQL 데이터베이스에 성공적으로 연결되었습니다.", "연결 성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Form admin = new admin_Menu(connection);
+                        admin.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("ID 또는 패스워드가 일치하지 않습니다.", "접속 실패");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("MySQL 데이터베이스에 연결할 수 없습니다.", "연결 실패", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("MySQL 데이터베이스 연결 중 오류가 발생했습니다.\n\n에러 메시지: " + ex.Message, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form new_acc = new new_account(connection);
+            new_acc.Show();
+            this.Hide();
+        }
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                loginDB(sender, e);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ C# winform/admin/prj_admin/new_account.cs
@@ -0,0 +1,122 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace prj_admin
+{
+    public partial class new_account : Form
+    {
+        MySqlConnection con;
+        public new_account(MySqlConnection con)
+        {
+            InitializeComponent();
+            PW.PasswordChar = '*';
+            PW.PasswordChar = '*';
+            this.con = con;
+        }
+        private void resisterUser(string id, string pw, string name, string email)
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            string duplicateQuery = "SELECT COUNT(*) FROM admin_info WHERE ID=@id AND email=@email";
+            using (MySqlCommand duplicateCmd = new MySqlCommand(duplicateQuery, con))
+            {
+                duplicateCmd.Parameters.AddWithValue("@id", id);
+                duplicateCmd.Parameters.AddWithValue("@email", email);
+                try
+                {
+                    int count = Convert.ToInt32(duplicateCmd.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        MessageBox.Show("이미 등록한 ID나 EMAIL 주소가 있습니다.", "회원가입");
+                    }
+                    else
+                    {
+                        string insertQuery = "INSERT INTO admin_info(ID, PW, name,email) VALUES(@id,@pw,@name,@email)";
+                        using (MySqlCommand insertCmd = new MySqlCommand(insertQuery, con))
+                        {
+                            insertCmd.Parameters.AddWithValue("@id", id);
+                            insertCmd.Parameters.AddWithValue("@pw", pw);
+                            insertCmd.Parameters.AddWithValue("@name", name);
+                            insertCmd.Parameters.AddWithValue("@email", email);
+
+                            int rowsAffected = insertCmd.ExecuteNonQuery();
+                            if (rowsAffected > 0)
+                            {
+                                MessageBox.Show("회원 가입이 완료되었습니다.", "회원가입");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("회원 가입 중 오류가 발생했습니다. 다시 시도해주세요.", "회원가입");
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        private bool ValidateInput(string id, string pw, string cpw, string email)
+        {
+            if (id.Length < 3 || id.Length > 13) //ID 형식 검사
+            {
+                MessageBox.Show("ID는 4자 이상 12자 이하여야 합니다.", "회원가입");
+                return false;
+            }
+            if (pw.Length < 3 || pw.Length > 13)//pw 형식 검사
+            {
+                MessageBox.Show("Password는 4자 이상 12자 이하여야 합니다.", "회원가입");
+                return false;
+            }
+            if (pw != cpw)
+            {
+                MessageBox.Show("비밀번호가 일치하지 않습니다.", "회원가입");
+                return false;
+            }
+            if (!IsValidEmail(email))
+            {
+                MessageBox.Show("유효한 이메일 주소를 입력하세요.", "회원가입");
+                return false;
+            }
+            return true;
+        }
+        private bool IsValidEmail(string email) //이메일 유효 검사
+        {
+            try
+            {
+                var eaddr = new System.Net.Mail.MailAddress(email);
+                return eaddr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput(ID.Text, PW.Text, REPW.Text, EMAIL.Text))
+            {
+                resisterUser(ID.Text, PW.Text, NAME.Text, EMAIL.Text);
+                this.Close();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form login = new login();
+            login.Show();
+        }
+    }
+}
